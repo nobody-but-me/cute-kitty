7f3b91c142c0a5d0a180ff0684a266a2c64058d4
Daniel Nunes Teixeira <nunesteixeiradaniel12@gmail.com>
2025-08-03 18:46:50 -0300

    The cute dawn.

---
+++ scripts/kitty.gd
@@ -0,0 +1,35 @@
+extends CharacterBody2D
+
+const ACCELERATION: float = .2
+const FRICTION: float = .04
+const GRAVITY: float = 1000.
+
+var JUMP_FORCE: float = 250.
+var SPEED: float = 100.
+
+func ready() -> void:
+	return
+
+func move() -> void:
+	var direction = Input.get_axis("a", "d")
+	
+	if (direction > .5): $sprite.flip_h = false
+	elif (direction < -.5): $sprite.flip_h = true
+	
+	if (direction != 0): 
+		self.velocity.x = lerp(self.velocity.x, direction * SPEED, ACCELERATION); 
+		if (self.is_on_floor()): $animation_player.play("walk")
+	else: 
+		self.velocity.x = lerp(self.velocity.x, .0, FRICTION); 
+		if (self.is_on_floor()): $animation_player.play("idle")
+	return
+
+func _process(_delta: float) -> void:
+	move()
+	self.move_and_slide()
+	self.velocity.y += GRAVITY * _delta
+	if (self.is_on_floor() || $jump_buffer.is_colliding()):
+		if (Input.is_action_pressed("jump")): self.velocity.y = -JUMP_FORCE; 
+		
+	if (!self.is_on_floor()): $animation_player.play("jump")
+	return
